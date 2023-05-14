32ad7dbda2db81fdb4dfd74a4966970c2afe96fe
unknown <luka.gavrilovic@elfak.rs>
2023-05-14 20:46:53 +0200

    AuthController.cs sa funkcijom CreatePassword

---
+++ Aplikacija/projekat/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace projekat.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    public NightEventsContext Context { get; set; }
+    private readonly IConfiguration _configuration;
+    public AuthController(NightEventsContext context, IConfiguration configuration)
+    {
+        Context = context;
+        _configuration = configuration;
+    }
+
+
+
+    private void CreatePasswordHash(string Password, out byte[] PasswordHash, out byte[] PasswordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            PasswordSalt = hmac.Key;
+            PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Password));
+        }
+    }
+
+
+
+    
+    
+}
